f57bde44ef264b3ab16807519fad4bf403d4c9c7
Zeke <zekeabshire@gmail.com>
2022-02-25 18:48:53 -0600

    organized into folders

--- src/column.py
+++
@@ -1,9 +0,0 @@
-class Column:
-    def __init__(self, width):
-        self.width = width
-        self.contents = 'abcdefghijklmnopqrstuvwxyz'
-
-    def render(self, line_number):
-        if line_number >= len(self.contents):
-            return ''
-        return self.contents[line_number].ljust(self.width)
